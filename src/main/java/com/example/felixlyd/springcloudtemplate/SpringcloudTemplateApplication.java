8a4882a4d01b8aa79f353b1ed4fc5de65f79ae8c
YaodongLiu <ttttttliu@qq.com>
2023-02-02 14:58:13 +0800

    初始化项目

---
+++ src/main/java/com/example/felixlyd/springcloudtemplate/SpringcloudTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.example.felixlyd.springcloudtemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudTemplateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/felixlyd/springcloudtemplate/SpringcloudTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.felixlyd.springcloudtemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudTemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
