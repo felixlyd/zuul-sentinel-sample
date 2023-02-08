a91b3bb452391091bd322d13631d919c58faef00
YaodongLiu <ttttttliu@qq.com>
2023-02-08 09:34:08 +0800

    自定义zuulFilter

---
+++ src/main/java/com/example/felixlyd/springcloudtemplate/config/ZuulConfig.java
@@ -0,0 +1,31 @@
+package com.example.felixlyd.springcloudtemplate.config;
+
+import com.example.felixlyd.springcloudtemplate.config.filter.filter.AdapterPostFilter;
+import com.example.felixlyd.springcloudtemplate.config.filter.filter.AdapterPreFilter;
+import com.netflix.zuul.ZuulFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+/**
+ * class classname
+ *
+ * @author : liuyaodong
+ * @date 2023/2/7
+ */
+@Configuration
+public class ZuulConfig {
+
+    @Bean
+    @Primary
+    public ZuulFilter adapterPreFilter() {
+        // We can also provider the filter order in the constructor.
+        return new AdapterPreFilter();
+    }
+
+    @Bean
+    @Primary
+    public ZuulFilter adapterPostFilter() {
+        return new AdapterPostFilter();
+    }
+}
---
+++ src/main/java/com/example/felixlyd/springcloudtemplate/config/filter/filter/AdapterPostFilter.java
@@ -0,0 +1,21 @@
+package com.example.felixlyd.springcloudtemplate.config.filter.filter;
+
+import com.alibaba.csp.sentinel.adapter.gateway.zuul.filters.SentinelZuulPostFilter;
+import com.netflix.zuul.exception.ZuulException;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * class classname
+ *
+ * @author : liuyaodong
+ * @date 2023/2/7
+ */
+@Slf4j
+public class AdapterPostFilter extends SentinelZuulPostFilter {
+    @Override
+    public Object run() throws ZuulException {
+        super.run();
+        log.info("--加密响应报文中--");
+        return null;
+    }
+}
